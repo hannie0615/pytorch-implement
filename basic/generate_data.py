f92cc240b65f389c24279dde11273fa0be9bba01
hannie <sksmsdhdn@naver.com>
2024-04-18 17:54:21 +0900

    mle 다항 회귀 작성, map는 작성 중

---
+++ basic/generate_data.py
@@ -0,0 +1,23 @@
+import math
+import numpy as np
+import random
+
+np.random.seed(42)
+
+def generate_data(num):
+
+    x = np.random.rand(num) * 2 - 1  # the range of [−1, 1]
+
+    # t = cos (2πx) + sin (πx) + ϵ
+    t = np.zeros(num)
+    for i in range(num):
+        t[i] = math.cos(2 * math.pi * x[i]) + math.sin(math.pi * x[i]) + random.gauss(0, 1 / 11.1)
+
+    return x, t
+
+def generate_weight(num):
+
+    w = np.zeros(num)
+    for i in range(num):
+        w[i] = random.gauss(0, 0.005)
+    return w
